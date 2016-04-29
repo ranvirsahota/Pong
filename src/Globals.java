0284463e5f992add24344f1e15cbfaf0fa7dc7cf
Ranvir Sahota <singhsahota@live.co.uk>
2016-04-29 12:21:11 +0100

    Inital commit

--- src/Globals.java
+++ src/Globals.java
@@ -2,7 +2,6 @@
 public class Globals {
 	public static int mouseY = 0;
 	public static boolean btnPlayClicked;
-
 	public static int ballX = 200;
 	public static int ballY = 10;
 	public static int aiY = (400/2) /50;
